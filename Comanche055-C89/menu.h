d30fd50c6aacb534d524f0c6299099c788845203
Julien QUENTIN <jqntn88@gmail.com>
2026-03-05 00:13:08 +0100

    refactor: extract menu into own module, clean up main.c

---
+++ Comanche055-C89/menu.h
@@ -0,0 +1,17 @@
+/*
+ * menu.h -- Backend selection menu.
+ *
+ * Presents an interactive console menu for choosing the DSKY
+ * display backend (console, Win32 GDI, or HTTP/SSE web).
+ *
+ * Comanche055 (Apollo 11 CM) ANSI C89 port.
+ */
+
+#ifndef MENU_H
+#define MENU_H
+
+#include "dsky_backend.h"
+
+dsky_backend_t *menu_select_backend(void);
+
+#endif /* MENU_H */
